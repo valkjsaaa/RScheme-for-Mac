64683e0a104865c1207840a110c57e9843798ac2
cuty-lewiwi <cuty-lewiwi@cutylewiwisMBP.lan>
2014-12-17 23:52:42 +0800

    GUI support

--- RScheme for Mac/ViewController.m
+++ RScheme for Mac/ViewController.m
@@ -10,6 +10,7 @@
 #import "RSchemeParser.h"
 #import "y.tab.h"
 #import "MessageBlocks.h"
+#import "RSchemeBox.h"
 
 #define OUTPUTLINE
 
@@ -17,10 +18,13 @@
 @property (strong, nonatomic) RSchemeParser *parser;
 @property (strong, nonatomic) NSMutableString *output;
 
+@property (nonatomic) NSPoint origin;
+
 @property (strong) IBOutlet NSTextView *inputTextField;
 @property (strong) IBOutlet NSTextView *outputTextView;
 @property (nonatomic) NSUInteger nextParseHeadIndex;
 //@property (nonatomic) NSUInteger nextResultHeadIndex;
+@property (strong) IBOutlet NSView *GUIView;
 @end
 
 @implementation ViewController
@@ -31,6 +35,8 @@
     self.output = [[NSMutableString alloc] initWithString:@""];
     self.parser = [[RSchemeParser alloc] initWithFileHandle:self.output];
     self.nextParseHeadIndex = 0;
+    RSchemeBox *test = [RSchemeBox newRSchemeBox];
+    //test.frame = NSRectFromCGRect(CGRectMake(10, 10, 108, 109));
     //self.nextResultHeadIndex = 0;
     // Do any additional setup after loading the view.
 }
@@ -45,6 +51,30 @@
 - (void)test{
     //NSString *str = [NSString stringWithUTF8String:yytext+2]
 }
+
+#pragma mark - IBActions
+
+- (IBAction)dragging:(NSPanGestureRecognizer *)sender
+{
+    //NSLog(@"dragging");
+    if (sender.state == NSGestureRecognizerStateBegan){
+        self.origin = sender.view.frame.origin;
+    }
+    else if (sender.state == NSGestureRecognizerStateChanged || sender.state == NSGestureRecognizerStateEnded){
+        NSPoint offset = [sender translationInView:self.GUIView];
+        sender.view.frame = NSRectFromCGRect(CGRectMake(self.origin.x+offset.x, self.origin.y+offset.y, sender.view.frame.size.width, sender.view.frame.size.height));
+    }
+}
+
+- (IBAction)addRschemeBoxView:(id)sender
+{
+    //RSchemeBox *newBox = [RSchemeBox newRSchemeBox];
+    RSchemeBox *newBox = [[RSchemeBox alloc] initWithFrame:NSRectFromCGRect(CGRectMake(10, 10, 108, 109))];
+    //newBox.frame = NSRectFromCGRect(CGRectMake(10, 10, 108, 109));
+    newBox.titleTextField.stringValue = @"haha";
+    [self.GUIView addSubview:newBox];
+}
+
 
 - (IBAction)parseButtonClicked:(NSButton *)sender
 {
